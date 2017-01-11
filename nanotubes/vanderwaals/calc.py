bf56392167e57d2b8dae006eeec1448771b4daee
iyudintsev <ivan.yudintsev18@gmail.com>
2017-01-12 01:04:07 +0300

    [nanotubes] add calc.py file

---
+++ nanotubes/vanderwaals/calc.py
@@ -0,0 +1,22 @@
+from lg_modified import force, energy
+
+
+def calc_force(p1, p2):
+    """
+    Force Calculation
+    :param p1: Particle
+    :param p2: Particle
+    """
+    f_lg = force(p1.r - p2.r)
+    p1.f_lg = f_lg
+    p2.f_lg = -f_lg
+
+
+def calc_energy(p1, p2):
+    """
+    Energy calculation
+    :param p1: Particle
+    :param p2: Particle
+    :return: double
+    """
+    return energy(p1.r - p2.r)
