f96011ae9e49b1c6fa570d365d5152f76c0a87a3
iyudintsev <ivan.yudintsev18@gmail.com>
2017-01-12 20:23:38 +0300

    [nanotubes] add nodeparticle & node

--- nanotubes/particle.py
+++ nanotubes/particle.py
@@ -1,14 +1,40 @@
-from vanderwaals.lg_modified import force
+import numpy as np
 
 
 class Particle(object):
     def __init__(self, r):
         self.r = r
-        self.v = None
-        self.f_bond = None
-        self.f_coul = None
-        self.f_coul_corr = None
-        self.f_lg = None
+        self.v = np.array([0., 0., 0.])
+        self.f_coul = np.array([0., 0., 0.])
+        self.f_coul_corr = np.array([0., 0., 0.])
+        self.f_lg = np.array([0., 0., 0.])
 
     def __repr__(self):
         return "<Particle {0}>".format(self.r)
+
+
+class NodeParticle(object):
+    def __init__(self, r, order=1):
+        """
+        :param r: numpy array
+        :param order: int,
+            0: first particle
+            1: simple particle
+            2: last particle
+        """
+        self.r = r
+        self.f_bond = np.array([0, 0, 0])
+        self.current_dist = np.array([0, 0])
+        self.next_dist = np.array([0, 0])
+        self.order = order
+
+    def __repr__(self):
+        return "<NodeParticle {0}>".format(self.r)
+
+
+class Node(object):
+    def __init__(self, p0, p1, p2):
+        self.particles = {0: p0, 1: p1, 2: p2}
+
+    def __getitem__(self, index):
+        return self.particles[index]
