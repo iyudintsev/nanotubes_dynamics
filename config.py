eb474d4cbcdf856b5b68ff326c75692b46ba2cfb
iyudintsev <ivan.yudintsev18@gmail.com>
2017-01-18 23:39:16 +0300

    [config] change repr format

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
-""" Coordinates Location """
+""" Configuration File """
 
+""" File with coordinates"""
 file_with_coor = "coordinates.txt"
 
 """ Model Parameters"""
-
 L = [1e-7 * x for x in [0, 200]]  # border
 
 """ Nanotube Parameters """
-
 k_bond = 5e3  # coefficient for bonding force and energy
